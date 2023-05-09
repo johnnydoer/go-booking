0f7affec317d218fa34f09c8a4252c885077a86a
JohnnyDoer <bajiraoayush@gmail.com>
2023-05-10 02:45:38 +0530

    implemeneted booking function

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ const conferenceTickets uint = 50
 var remainingTickets uint = 50
 var conferenceName string = "Go Conference"
 
+var bookings = []UserData{}
+
+type UserData struct {
+	firstName, lastName, email string
+	numberOfTickets            uint
+}
+
 func main() {
 	// Greet users to the booking system.
 	greetUsers()
@@ -21,21 +28,24 @@ func main() {
 	// Validate the input.
 	isValidName, isValidEmail, isValidTickets := helper.ValidateData(firstName, lastName, email, userTickets, remainingTickets)
 
-	if isValidName && isValidEmail && isValidTickets {
-		fmt.Println("Data is valid.")
+	if !(isValidName && isValidEmail && isValidTickets) {
+		fmt.Println("Data is invalid.")
 	}
 
-	// Book the tickets
+	// Book the tickets.
+	bookTickets(firstName, lastName, email, userTickets)
 
 	// Print booking information.
 
 	// Email the booking information asynchronously.
 
-	// Use switch case for city,
+	// Use switch case for city.
 }
 
 func greetUsers() {
-	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend:\n", conferenceName, conferenceTickets, remainingTickets)
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend:\n")
 }
 
 func getUserData() (string, string, string, uint) {
@@ -43,16 +53,35 @@ func getUserData() (string, string, string, uint) {
 	var userTickets uint
 
 	fmt.Printf("Enter first name: ")
-	fmt.Scanln(&firstName)
+	fmt.Scan(&firstName)
 
 	fmt.Printf("Enter last name: ")
-	fmt.Scanln(&lastName)
+	fmt.Scan(&lastName)
 
 	fmt.Printf("Enter email ID: ")
-	fmt.Scanln(&email)
+	fmt.Scan(&email)
 
 	fmt.Printf("Enter number of tickets you want to purchase: ")
-	fmt.Scanln(&userTickets)
+	fmt.Scan(&userTickets)
 
 	return firstName, lastName, email, userTickets
+}
+
+func bookTickets(firstName, lastName, email string, userTickets uint) {
+	remainingTickets = remainingTickets - userTickets
+
+	userData := UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+
+	fmt.Println("All bookings:\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v remaining tickets.\n", remainingTickets)
 }
